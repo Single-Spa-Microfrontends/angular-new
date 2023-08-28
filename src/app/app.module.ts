819938831fc45ce8a4982a587246ce9ac6f8ea97
Oleg Kuibar <oleg.kuibar@a4media.com>
2023-08-28 00:47:17 -0400

    [MFA]: Update Angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { APP_BASE_HREF } from "@angular/common";
 import { EmptyRouteComponent } from "./empty-route/empty-route.component";
 
 @NgModule({
